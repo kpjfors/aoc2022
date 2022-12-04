120358b9548cabfdff8f76ab9a83df0c8ee5e0cd
kpjfors <kpjfors@protonmail.com>
2022-12-04 16:16:24 +0100

    day 4

---
+++ 4/4a.py
@@ -0,0 +1,17 @@
+def main(inp):
+    overlapping = 0
+    for i in inp:
+        first, second = i.strip().split(",")
+        first = [int(x) for x in first.split("-")]
+        second = [int(x) for x in second.split("-")]
+        if first[0]<=second[0] and first[1]>=second[1]:
+            overlapping +=1
+        elif first[0]>=second[0] and first[1]<=second[1]:
+            overlapping +=1
+    return overlapping
+
+
+if __name__ == "__main__":
+    f = open("4/input.txt", "r")
+    inp = f.readlines()
+    print(main(inp))
