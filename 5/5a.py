4b0cf2a5209cb86a736a29c1622c8c679015638c
kpjfors <kpjfors@protonmail.com>
2022-12-07 01:23:44 +0100

    day 5 & 6

---
+++ 5/5a.py
@@ -0,0 +1,29 @@
+def main(inp):
+    inp_size_stack = 4
+    num_stacks = int(len(inp[0])/inp_size_stack)
+    stacks = [[] for x in range(num_stacks)]
+    stacks_built = False
+    for line in inp:
+        if len(line)>1 and line.strip()[0] == "[": #only true for buid instructions
+            for stack in range(num_stacks):
+                crate = line[int(stack*inp_size_stack):int((stack+1)*inp_size_stack)]
+                if crate.strip() == "":
+                    pass
+                else:
+                    stacks[stack].append(crate[1:-2])
+        elif stacks_built == False:
+            for stack in stacks:
+                stack.reverse()
+            stacks_built = True
+        if line[0] == "m": #only true for move instructions
+            unparsed = line.strip().split(" ")
+            cnt, fr, to = [int(x) for x in unparsed[1::2]]
+            for i in stacks[fr-1][-cnt:][::-1]:
+                stacks[to-1].append(i)
+            stacks[fr-1] = stacks[fr-1][:-cnt]
+    return "".join([stack[-1] for stack in stacks])
+
+if __name__ == "__main__":
+    f = open("5/input.txt", "r")
+    inp = f.readlines()
+    print(main(inp))
---
+++ 6/6a.py
@@ -0,0 +1,13 @@
+def main(inp):
+    seq = inp[0]
+    i = 0
+    while 1:
+        substr = seq[i:i+4]
+        if len(set(substr)) == len(substr):
+            return i+4 
+        i += 1
+
+if __name__ == "__main__":
+    f = open("6/input.txt", "r")
+    inp = f.readlines()
+    print(main(inp))
