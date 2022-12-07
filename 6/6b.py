4c8749325f5929fe033289f21f912037ae053bb6
kpjfors <kpjfors@protonmail.com>
2022-12-07 01:22:03 +0100

    day 5 & 6

---
+++ 6/6b.py
@@ -0,0 +1,13 @@
+def main(inp):
+    seq = inp[0]
+    i = 0
+    while 1:
+        substr = seq[i:i+14]
+        if len(set(substr)) == len(substr):
+            return i+14 
+        i += 1
+
+if __name__ == "__main__":
+    f = open("6/input.txt", "r")
+    inp = f.readlines()
+    print(main(inp))
